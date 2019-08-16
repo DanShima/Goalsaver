3555ebf3f68d542184311bde88103852a90506e5
Danshima <danning.cc@gmail.com>
2019-08-16 23:52:31 +0200

    added some files from google sample code

--- app/src/main/java/com/danshima/savemyq/network/NetworkBounceResource.kt
+++ app/src/main/java/com/danshima/savemyq/network/NetworkBounceResource.kt
@@ -1,3 +1,149 @@
 package com.danshima.savemyq.network
 
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.danshima.savemyq.AppExecutors
+import retrofit2.Response
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
+
+/**
+ * A generic class that can provide a resource backed by both the sqlite database and the network.
+ *
+ *
+ * You can read more about it in the [Architecture
+ * Guide](https://developer.android.com/arch).
+ * @param <ResultType>
+ * @param <RequestType>
+</RequestType></ResultType> */
+abstract class NetworkBoundResource<ResultType, RequestType>
+@MainThread constructor(private val appExecutors: AppExecutors) {
+
+    private val result = MediatorLiveData<Resource<ResultType>>()
+
+    init {
+        result.value = Resource.Loading(null)
+        @Suppress("LeakingThis")
+        val dbSource = loadFromDb()
+        result.addSource(dbSource) { data ->
+            result.removeSource(dbSource)
+            if (shouldFetch(data)) {
+                fetchFromNetwork(dbSource)
+            } else {
+                result.addSource(dbSource) { newData ->
+                    setValue(Resource.Success(newData))
+                }
+            }
+        }
+    }
+
+    @MainThread
+    private fun setValue(newValue: Resource<ResultType>) {
+        if (result.value != newValue) {
+            result.value = newValue
+        }
+    }
+
+    private fun fetchFromNetwork(dbSource: LiveData<ResultType>) {
+        val apiResponse = createCall()
+        // we re-attach dbSource as a new source, it will dispatch its latest value quickly
+        result.addSource(dbSource) { newData ->
+            setValue(Resource.Loading(newData))
+        }
+        result.addSource(apiResponse) { response ->
+            result.removeSource(apiResponse)
+            result.removeSource(dbSource)
+            when (response) {
+                is ApiSuccessResponse -> {
+                    appExecutors.diskIO().execute {
+                        saveCallResult(processResponse(response))
+                        appExecutors.mainThread().execute {
+                            // we specially request a new live data,
+                            // otherwise we will get immediately last cached value,
+                            // which may not be updated with latest results received from network.
+                            result.addSource(loadFromDb()) { newData ->
+                                setValue(Resource.Success(newData))
+                            }
+                        }
+                    }
+                }
+                is ApiEmptyResponse -> {
+                    appExecutors.mainThread().execute {
+                        // reload from disk whatever we had
+                        result.addSource(loadFromDb()) { newData ->
+                            setValue(Resource.Success(newData))
+                        }
+                    }
+                }
+                is ApiErrorResponse -> {
+                    onFetchFailed()
+                    result.addSource(dbSource) { newData ->
+                        setValue(Resource.Error(response.errorMessage, newData))
+                    }
+                }
+            }
+        }
+    }
+
+    protected open fun onFetchFailed() {}
+
+    fun asLiveData() = result as LiveData<Resource<ResultType>>
+
+    @WorkerThread
+    protected open fun processResponse(response: ApiSuccessResponse<RequestType>) = response.body
+
+    @WorkerThread
+    protected abstract fun saveCallResult(item: RequestType)
+
+    @MainThread
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    @MainThread
+    protected abstract fun loadFromDb(): LiveData<ResultType>
+
+    @MainThread
+    protected abstract fun createCall(): LiveData<ApiResponse<RequestType>>
+}
+
+
+
+sealed class ApiResponse<T> {
+    companion object {
+        fun <T> create(error: Throwable): ApiErrorResponse<T> {
+            return ApiErrorResponse(error.message ?: "unknown error")
+        }
+
+        fun <T> create(response: Response<T>): ApiResponse<T> {
+            return if (response.isSuccessful) {
+                val body = response.body()
+                if (body == null || response.code() == 204) {
+                    ApiEmptyResponse()
+                } else {
+                    ApiSuccessResponse(
+                        body = body
+                    )
+                }
+            } else {
+                val msg = response.errorBody()?.string()
+                val errorMsg = if (msg.isNullOrEmpty()) {
+                    response.message()
+                } else {
+                    msg
+                }
+                ApiErrorResponse(errorMsg ?: "unknown error")
+            }
+        }
+    }
+}
+
+/**
+ * separate class for HTTP 204 responses so that we can make ApiSuccessResponse's body non-null.
+ */
+class ApiEmptyResponse<T> : ApiResponse<T>()
+data class ApiSuccessResponse<T>(val body: T) : ApiResponse<T>()
+data class ApiErrorResponse<T>(val errorMessage: String) : ApiResponse<T>()
--- app/src/main/java/com/danshima/savemyq/network/Resource.kt
+++ app/src/main/java/com/danshima/savemyq/network/Resource.kt
@@ -3,8 +3,14 @@ package com.danshima.savemyq.network
 import android.content.Context
 import android.net.ConnectivityManager
 
-sealed class Resource<out T> {
-    class Loading<out T> : Resource<T>()
-    data class Success<out T>(val data: T?) : Resource<T>()
-    data class Failure<out T>(val throwable: Throwable) : Resource<T>()
+// A generic class that contains data and status about loading this data.
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
 }
+
+
